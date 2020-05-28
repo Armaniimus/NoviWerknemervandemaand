bd1b8f5d04392900cf92b7ac3e98a863a9b7753d
Armaniimus_home <Tommy_chester@hotmail.com>
2020-05-28 15:35:50 +0200

    added toastShow function

--- app/src/main/java/com/example/noviwerknemervandemaand/Logger.java
+++ app/src/main/java/com/example/noviwerknemervandemaand/Logger.java
@@ -1,6 +1,8 @@
 package com.example.noviwerknemervandemaand;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Logger {
     private int activityID;
@@ -11,5 +13,12 @@ public class Logger {
     public void switchActivity(int newActivityID) {
         String msg = "CurrentActivityID: " + this.activityID + "==> NewActivityID: " + newActivityID;
         Log.v("switchView", msg);
+    }
+
+    public void toastShow(Context context,  CharSequence text) {
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
